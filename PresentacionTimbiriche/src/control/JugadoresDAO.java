932d64fe951f79f1bebaa4ad06664ed9735cc596
BryanS <bryanisaac77@hotmail.com>
2021-11-22 09:28:50 -0700

    Avance 8 proyecto

---
+++ PresentacionTimbiriche/src/control/JugadoresDAO.java
@@ -0,0 +1,95 @@
+/**
+ * Paquete control
+ */
+package control;
+
+/**
+ * Imports utilizados
+ */
+import dominio.ColorJ;
+import dominio.Jugador;
+
+/**
+ * Clase JugadoresDAO que se utiliza para hacer uso de un arreglo de los
+ * jugadores
+ *
+ */
+public class JugadoresDAO {
+
+    /**
+     * Atributos de clase
+     */
+    public static Jugador[] jugadores;
+
+    /**
+     * Constructor que no recibe nada en sus parametros
+     */
+    public JugadoresDAO() {
+        jugadores = new Jugador[4];
+        Jugador jugador1 = new Jugador();
+        jugador1.setColor(ColorJ.VERDE);
+        jugador1.setPuntuacion(0);
+        Jugador jugador2 = new Jugador("Nata Cano",
+                null,
+                ColorJ.ROJO,
+                0);
+        Jugador jugador3 = new Jugador("God Bunny",
+                null,
+                ColorJ.INDIGO,
+                0);
+        Jugador jugador4 = new Jugador("Yoda",
+                null,
+                ColorJ.AZUL,
+                0);
+        jugadores[0] = jugador1;
+        jugadores[1] = jugador2;
+        jugadores[2] = jugador3;
+        jugadores[3] = jugador4;
+    }
+
+    /**
+     * Método que agrega al jugador número 1
+     *
+     * @param jugador Jugador a agregar
+     */
+    public void agregarJugador1(Jugador jugador) {
+        this.jugadores[0] = jugador;
+    }
+
+    /**
+     * Método que agrega al jugador número 2
+     *
+     * @param jugador Jugador a agregar
+     */
+    public void agregarJugador2(Jugador jugador) {
+        this.jugadores[1] = jugador;
+    }
+
+    /**
+     * Método que agrega al jugador número 3
+     *
+     * @param jugador Jugador a agregar
+     */
+    public void agregarJugador3(Jugador jugador) {
+        this.jugadores[2] = jugador;
+    }
+
+    /**
+     * Método que agrega al jugador número 4
+     *
+     * @param jugador Jugador a agregar
+     */
+    public void agregarJugador4(Jugador jugador) {
+        this.jugadores[3] = jugador;
+    }
+
+    /**
+     * Se obtienen los datos de los jugadores.
+     *
+     * @return Arreglo de jugadores
+     */
+    public Jugador[] obtenerJugadores() {
+        return this.jugadores;
+    }
+
+}
---
+++ PresentacionTimbiriche/src/main/Main.java
@@ -0,0 +1,56 @@
+/**
+ * Paquete main
+ */
+package main;
+
+/**
+ * Imports utilizados
+ */
+import control.JugadoresDAO;
+import presentacion.FrmMenuPrincipal;
+
+/**
+ * Clase main en donde se ejecutara el inicio del programa
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        JugadoresDAO jugadoresDAO = new JugadoresDAO();
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrmMenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrmMenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrmMenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrmMenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FrmMenuPrincipal().setVisible(true);
+            }
+        });
+    }
+
+}
